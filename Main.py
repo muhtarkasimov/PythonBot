77d3bf9542f43b12eb6c98d348b9f7d592d21cd0
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-03 19:07:44 +0600

    Creating Bot via Python & Telegram Bot works and answers to '/start' command Bot answers only when code is running and not stopped

---
+++ Main.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot = telebot.TeleBot('1019588003:AAG0AVRVX7s0AapaeJyhNR66e_CIVrYmR60')
+
+@bot.message_handler(commands=['start'])
+
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты мне написал /start')
+
+
+
+
+
+bot.polling()
+
+
+
