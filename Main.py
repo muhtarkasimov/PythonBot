d31d26c57ccc51de9c59b8d104ee0f4357a3f3ce
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-04 13:33:53 +0600

    Bot answers to 'Привет' & 'Пока'

--- Main.py
+++ Main.py
@@ -3,10 +3,15 @@ import telebot
 bot = telebot.TeleBot('1019588003:AAG0AVRVX7s0AapaeJyhNR66e_CIVrYmR60')
 
 @bot.message_handler(commands=['start'])
-
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты мне написал /start')
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
 
 bot.polling()
 
