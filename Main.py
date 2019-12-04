54c56b26d30afc794b06a2168ecd2ffae6e6c596
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-04 14:43:12 +0600

    Added OWM (open weather map) but its weather is not accurate Searching new API

--- Main.py
+++ Main.py
@@ -1,4 +1,19 @@
 import telebot
+# import pyowm
+
+# HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6 - AccuWeather API key
+
+# --------------------------------------------------
+# Open Weather Map API code
+# owm = pyowm.OWM('200075eea62023528eb46334e61a76c1')
+# observation = owm.weather_at_place('Bishkek')
+# # print(observation)
+# weather = observation.get_weather()
+# temperature = weather.get_temperature('celsius')['temp']
+# print(temperature)
+# wind = weather.get_wind()
+# print(wind)
+# ----------------------------------------------------
 
 bot = telebot.TeleBot('1019588003:AAG0AVRVX7s0AapaeJyhNR66e_CIVrYmR60')
 
