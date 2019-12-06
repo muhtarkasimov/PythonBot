075580d2be80b14cd24cc10b171e50bcf1a23264
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-06 19:36:04 +0600

    Adding to Weather.py class Weather Adding get_weather_in_text method

--- Weather.py
+++ Weather.py
@@ -2,43 +2,57 @@ import time
 import json
 import urllib.request
 
+
 # HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6 - AccuWeather API key
 # Bishkek code - 222844
 
-API = 'HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6'
-country_code = 'KG'
-city = 'Bishkek'
+class Weather:
 
-key=''
-def getLocation(country_code, city):
-    search_address = 'http://dataservice.accuweather.com/locations/v1/cities/' + country_code + '/search?apikey=HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6&q=' + city
-    # print(search_address)
-    with urllib.request.urlopen(search_address) as search_address:
-        data = json.loads((search_address.read().decode()))
-    # print(data)
-    location_key = data[0]['Key']
-    return location_key
+    def __init__(self):
+        self.API = 'HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6'
+        self.country_code = 'KG'
+        self.city = 'Bishkek'
+        self.key = ''
+        self.forecast = ''
+        self.location_key = ''
+        self.forecast = ''
 
-def getForcast(location_key):
-    daily_forcastUrl = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + location_key + '?apikey=HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6'
-    with urllib.request.urlopen(daily_forcastUrl) as daily_forcastUrl:
-        data = json.loads(daily_forcastUrl.read().decode())
-    # print(data)
-    return data
+    def getLocation(self):
+        search_address = 'http://dataservice.accuweather.com/locations/v1/cities/' + self.country_code + '/search?apikey=HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6&q=' + self.city
+        # print(search_address)
+        with urllib.request.urlopen(search_address) as search_address:
+            data = json.loads((search_address.read().decode()))
+        # print(data)
+        self.location_key = data[0]['Key']
 
-key = getLocation(country_code, city)
-forecast = getForcast(key)
+    def getForcast(self):
+        daily_forcastUrl = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + self.location_key + '?apikey=HCgKwXyCl8E6ZqKV3iXfGAqPt1IKxPr6'
+        with urllib.request.urlopen(daily_forcastUrl) as daily_forcastUrl:
+            data = json.loads(daily_forcastUrl.read().decode())
+        # print(data)
+        self.forecast = data
 
-min_temp = forecast['DailyForecasts'][0]['Temperature']['Minimum']['Value']
-print('min temp: ' + str(min_temp))
-max_temp = forecast['DailyForecasts'][0]['Temperature']['Maximum']['Value']
-print('max temp: ' + str(max_temp))
-day_precipitation = forecast['DailyForecasts'][0]['Day']['HasPrecipitation']
-print('day precipitation: ' + str(day_precipitation))
-day_sky = forecast['DailyForecasts'][0]['Day']['IconPhrase']
-print('day sky: ' + str(day_sky))
-night_precipitation = forecast['DailyForecasts'][0]['Night']['HasPrecipitation']
-print('night precipitation: ' + str(night_precipitation))
-night_sky = forecast['DailyForecasts'][0]['Night']['IconPhrase']
-print('day sky: ' + str(night_sky))
+    def get_weather_in_text(self):
+        self.getLocation()
+        self.getForcast()
+
+        min_temp = self.forecast['DailyForecasts'][0]['Temperature']['Minimum']['Value']
+        max_temp = self.forecast['DailyForecasts'][0]['Temperature']['Maximum']['Value']
+        day_precipitation = self.forecast['DailyForecasts'][0]['Day']['HasPrecipitation']
+        day_sky = self.forecast['DailyForecasts'][0]['Day']['IconPhrase']
+        night_precipitation = self.forecast['DailyForecasts'][0]['Night']['HasPrecipitation']
+        night_sky = self.forecast['DailyForecasts'][0]['Night']['IconPhrase']
+
+        weather_array = [min_temp, max_temp, day_precipitation, day_sky, night_precipitation, night_sky]
+        return weather_array
+
+        # print('min temp: ' + str(min_temp))
+        # print('max temp: ' + str(max_temp))
+        # print('day precipitation: ' + str(day_precipitation))
+        # print('day sky: ' + str(day_sky))
+        # print('night precipitation: ' + str(night_precipitation))
+        # print('day sky: ' + str(night_sky))
+
+main_wewather = Weather()
+print(main_wewather.get_weather_in_text())
 
